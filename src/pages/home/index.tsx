5deea6abc20e6ebf1d5d40652f6a2ccf7554e766
Nicolly Rocha <nicollyrcs.dev@gmail.com>
2026-03-09 18:51:30 -0300

    ajeitando pt no mobile

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { Timeline } from "../../components/Timeline";
 
 export const Home = () => {
   return (
-    <div className="p-8 flex flex-col items-center gap-5">
+    <div className="p-8 pt-18 md:pt-8 flex flex-col items-center gap-5">
       <h2 className="text-3xl font-semibold mb-4">Timeline</h2>
       <Timeline />
     </div>
